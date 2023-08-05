6c2e7c74d2d43a26d36787dc429de6b20473f989
Alsey Coleman Miller <alseycmiller@gmail.com>
2023-08-05 09:24:21 -0400

    Renamed `RPCVersion`

--- Sources/LBRY/Networking/RPCVersion.swift
+++ Sources/LBRY/Networking/RPCVersion.swift
@@ -1,5 +1,5 @@
 //
-//  JSONRPCVersion.swift
+//  RPCVersion.swift
 //  
 //
 //  Created by Alsey Coleman Miller on 8/5/23.
@@ -8,7 +8,7 @@
 import Foundation
 
 /// Version of the [JSON-RPC](https://en.wikipedia.org/wiki/JSON-RPC) protocol
-public enum JSONRPCVersion: String, Codable {
+public enum RPCVersion: String, Codable {
     
     case v1_0 = "1.0"
     case v1_1 = "1.1"
