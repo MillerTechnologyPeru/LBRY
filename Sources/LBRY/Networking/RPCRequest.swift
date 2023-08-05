4b6922332a05a7bc72db58003217307c57d1e551
Alsey Coleman Miller <alseycmiller@gmail.com>
2023-08-05 07:15:43 -0400

    Add `RPCRequest`

---
+++ Sources/LBRY/Networking/RPCRequest.swift
@@ -0,0 +1,61 @@
+//
+//  RPCRequest.swift
+//  
+//
+//  Created by Alsey Coleman Miller on 8/5/23.
+//
+
+import Foundation
+#if canImport(FoundationNetworking)
+import FoundationNetworking
+#endif
+
+/// RPC Request
+public struct RPCRequest <Parameters>: Identifiable {
+    
+    public var version: JSONRPCVersion
+    
+    public let id: UInt
+    
+    public var method: RPCMethod
+    
+    public var parameters: Parameters
+    
+    enum CodingKeys: String, CodingKey {
+        
+        case version = "jsonrpc"
+        case method
+        case id
+        case parameters = "params"
+    }
+}
+
+extension RPCRequest: Encodable where Parameters: Encodable { }
+
+extension RPCRequest: Decodable where Parameters: Decodable { }
+
+extension RPCRequest: Equatable where Parameters: Equatable { }
+
+extension RPCRequest: Hashable where Parameters: Hashable { }
+
+// MARK: - URL Request
+
+public extension URLRequest {
+    
+    init<Parameters>(
+        request: RPCRequest<Parameters>,
+        server: LBRYServer,
+        token: AuthorizationToken? = nil
+    ) throws where Parameters: Encodable {
+        let encoder = JSONEncoder.lbry
+        let data = try encoder.encode(request)
+        self.init(url: server.url)
+        self.httpMethod = HTTPMethod.post.rawValue
+        self.httpBody = data
+        self.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        self.addValue("application/json", forHTTPHeaderField: "Accept")
+        if let token = token {
+            self.setAuthorization(token)
+        }
+    }
+}
