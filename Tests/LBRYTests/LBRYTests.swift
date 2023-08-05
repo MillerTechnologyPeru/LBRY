610a96fdd9a1d19ffa78ce7a12a9331c2f065ba1
Alsey Coleman Miller <alseycmiller@gmail.com>
2023-08-05 07:16:17 -0400

    Updated unit tests

--- Tests/LBRYTests/LBRYTests.swift
+++ Tests/LBRYTests/LBRYTests.swift
@@ -7,5 +7,7 @@ final class LBRYTests: XCTestCase {
         
         XCTAssertEqual(LBRYServer.odysee.rawValue, "https://api.na-backend.odysee.com/api/v1/proxy")
         XCTAssertEqual(LBRYServer.localhost(), LBRYServer(rawValue: "http://localhost:5279"))
+        XCTAssertEqual(LBRYServer.localhost().url.port, 5279)
+        XCTAssertEqual(LBRYServer.localhost(port: 1234).url.port, 1234)
     }
 }
