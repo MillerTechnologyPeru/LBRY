ad32f3f2e0ca4f082cd73581be180975778d4533
Alsey Coleman Miller <alseycmiller@gmail.com>
2023-08-05 06:37:42 -0400

    Updated unit tests

--- Tests/LBRYTests/LBRYTests.swift
+++ Tests/LBRYTests/LBRYTests.swift
@@ -3,7 +3,9 @@ import XCTest
 
 final class LBRYTests: XCTestCase {
     
-    func testExample() throws {
+    func testServerURL() throws {
         
+        XCTAssertEqual(LBRYServer.odysee.rawValue, "https://api.na-backend.odysee.com/api/v1/proxy")
+        XCTAssertEqual(LBRYServer.localhost(), LBRYServer(rawValue: "http://localhost:5279"))
     }
 }
